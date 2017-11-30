7b199be19fb9ca267fc578bebeb4b69d6596430b
Micah Johnson <info@micahjjohnson.com>
2017-11-30 13:22:08 -0600

    Full Production and my personal copy
    Full Production and my personal copy

--- main.js
+++ main.js
@@ -1,23 +1,58 @@
-Date.prototype.addDays = function(days) {
-    var maxDays = new Date(this.valueOf());
-    maxDays.setDate(maxDays.getDate() + days);
-    return maxDays;
-}
-
-var maxDays = new Date();
+ Date.prototype.addDays = function(days) {
+     var maxDays = new Date(this.valueOf());
+     maxDays.setDate(maxDays.getDate() + days);
+     return maxDays;
+ }
 
 
-var i = $("#build");
-if (maxDays.getDay() !== 0) {
-    i.append(maxDays.addDays(8).toDateString());
-} else if (maxDays.getDay() == 6) {
-    i.append(maxDays.addDays(9).toDateString());
-} else {
-    i.append(maxDays.addDays(7).toDateString());
-}
+ var maxDays = new Date();
+ var j = $("#build");
+ var i = $("#complete");
 
-$("#complete").append(maxDays.addDays(4).toDateString());
-$("#moneyBack").append(maxDays.addDays(100).toDateString());
+ switch (maxDays.getDay()) {
+     case 0:
+         i.append(maxDays.addDays(4).toDateString());
+         break;
+     case 1:
+         i.append(maxDays.addDays(4).toDateString());
+         break;
+     case 2:
+         i.append(maxDays.addDays(6).toDateString());;
+         break;
+     case 3:
+         i.append(maxDays.addDays(6).toDateString());
+         break;
+     case 4:
+         i.append(maxDays.addDays(6).toDateString());
+         break;
+     case 5:
+         i.append(maxDays.addDays(6).toDateString());
+         break;
+     case 6:
+         i.append(maxDays.addDays(5).toDateString());
+ }
 
+ switch (maxDays.getDay()) {
+     case 0:
+         j.append(maxDays.addDays(9).toDateString());
+         break;
+     case 1:
+         j.append(maxDays.addDays(9).toDateString());
+         break;
+     case 2:
+         j.append(maxDays.addDays(9).toDateString());;
+         break;
+     case 3:
+         j.append(maxDays.addDays(9).toDateString());
+         break;
+     case 4:
+         j.append(maxDays.addDays(11).toDateString());
+         break;
+     case 5:
+         j.append(maxDays.addDays(11).toDateString());
+         break;
+     case 6:
+         j.append(maxDays.addDays(10).toDateString());
+ }
 
-console.log("hello");
+ $("#moneyBack").append(maxDays.addDays(100).toDateString());
